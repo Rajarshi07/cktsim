7b9c183b40c9a4f951fcccccbe951542f68991f1
Rajarshi Ghosh Dastidar <35489362+Rajarshi07@users.noreply.github.com>
2020-09-12 12:43:34 +0530

    Simple Gates Added

--- cktsim/sim/urls.py
+++ cktsim/sim/urls.py
@@ -3,8 +3,16 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('allckts/', views.allckts, name='allckts'),
-    path('hadd/', views.hadd, name='halfadder'),
-    path('fadd/', views.fadd, name='fulladder'),
-    path('hsub/', views.hsub, name='halfsubstractor'),
-    path('fsub/', views.fsub, name='fullsubstractor'),
+    path('adder/hadd/', views.hadd, name='halfadder'),
+    path('adder/fadd/', views.fadd, name='fulladder'),
+    path('subtractor/hsub/', views.hsub, name='halfsubstractor'),
+    path('subtractor/fsub/', views.fsub, name='fullsubstractor'),
+    path('gate/and/', views.gate_and ,name='gate_and'),
+    path('gate/or/', views.gate_or ,name='gate_or'),
+    path('gate/xor/', views.gate_xor ,name='gate_xor'),
+    path('gate/xnor/', views.gate_xnor ,name='gate_xnor'),
+    path('gate/nand/', views.gate_nand ,name='gate_nand'),
+    path('gate/nor/', views.gate_nor ,name='gate_nor'),
+    path('gate/not/', views.gate_not ,name='gate_not'),
+
 ]
--- cktsim/sim/views.py
+++ cktsim/sim/views.py
@@ -12,3 +12,17 @@ def hsub(request):
     return render(request,'hsub.html')
 def fsub(request):
     return render(request,"fsub.html")
+def gate_and(request):
+    return render(request,"gate_and.html")
+def gate_or(request):
+    return render(request,"gate_or.html")
+def gate_xor(request):
+    return render(request,"gate_xor.html")
+def gate_xnor(request):
+    return render(request,"gate_xnor.html")
+def gate_nand(request):
+    return render(request,"gate_nand.html")
+def gate_nor(request):
+    return render(request,"gate_nor.html")
+def gate_not(request):
+    return render(request,"gate_not.html")
